baf725a0ed1610db90138b3d13984833040743da
FazleRahmanEvan <evanshakib26@gmail.com>
2024-05-23 14:13:59 +0600

    interface model

---
+++ src/app/modules/product/product.model.ts
@@ -0,0 +1,57 @@
+import mongoose, { Schema, model, connect } from 'mongoose';
+import Product, { Inventory, Variant } from './product.interface';
+
+const variantSchema = new Schema<Variant>({
+  type: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: String,
+    required: true,
+  },
+});
+
+const inventorySchema = new Schema<Inventory>({
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  inStock: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const productSchema = new Schema<Product>({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Description is required'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'Price is required'],
+  },
+  category: {
+    type: String,
+    required: [true, 'Category is required'],
+  },
+  tags: {
+    type: [String],
+    required: true,
+  },
+  variants: {
+    type: [variantSchema],
+    required: true,
+  },
+  inventory: {
+    type: inventorySchema,
+    required: true,
+  },
+});
+
+export const Product = mongoose.model('Product', productSchema);
