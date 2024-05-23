2fed1b9969fc82383b0aa5cc1d3d4580e01eda97
FazleRahmanEvan <evanshakib26@gmail.com>
2024-05-23 12:30:08 +0600

    updated

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,16 @@
-import app from "./app";
-import config from "./app/config";
-
-import mongoose from'mongoose';
+import mongoose from 'mongoose';
+import app from './app';
+import config from './app/config';
 
 async function main() {
-    try{
-        await mongoose.connect(process.env.DATABASE_URL as string);
-
-        app.listen(config.port, () => {
-            console.log(`Example app listening on port ${config.port}`)
-          })
-    }catch(err){
-        console.log(err)
-    }
-   
+  try {
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 main();
