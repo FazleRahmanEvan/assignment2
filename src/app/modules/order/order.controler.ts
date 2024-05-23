90dac17dc2bf60f1c8585310603972d85f32b6f6
FazleRahmanEvan <evanshakib26@gmail.com>
2024-05-23 23:20:40 +0600

    stock

--- src/app/modules/order/order.controler.ts
+++ src/app/modules/order/order.controler.ts
@@ -1,14 +1,38 @@
 import { Request, Response } from 'express';
 import { OrderServices } from './order.service';
-const createProduct = async (req: Request, res: Response) => {
-  const productData = req.body;
-  const result = await OrderServices.createOrder(productData);
 
-  res.json({
-    success: true,
-    message: 'Order is placed successfully',
-    data: result,
-  });
+const createProduct = async (req: Request, res: Response) => {
+  //   const productData = req.body;
+  //   const result = await OrderServices.createOrder(productData);
+
+  //   res.json({
+  //     success: true,
+  //     message: 'Order is placed successfully',
+  //     data: result,
+  //   });
+
+  const orderData = req.body;
+  try {
+    const result = await OrderServices.createOrder(orderData);
+
+    if (result) {
+      res.json({
+        success: true,
+        message: 'Order is placed successfully',
+        data: result,
+      });
+    } else {
+      res.json({
+        success: false,
+        message: ` Insufficient product in stock`,
+      });
+    }
+  } catch (error: any) {
+    res.json({
+      success: false,
+      message: `Order Failed: ${error.message}`,
+    });
+  }
 };
 // const retrieveAllOrders = async (req: Request, res: Response) => {
 //   const result = await OrderServices.retrieveAllOrders();
--- src/app/modules/order/order.service.ts
+++ src/app/modules/order/order.service.ts
@@ -1,9 +1,29 @@
+import { Product } from '../product/product.model';
 import { EOrder } from './order.interface';
 import Order from './order.model';
 
 const createOrder = async (payload: EOrder) => {
-  const result = await Order.create(payload);
-  return result;
+  //   const result = await Order.create(payload);
+  //   return result;
+  const productId = payload.productId;
+
+  const productOld = await Product.findOne({ _id: productId });
+
+  if (
+    productOld?.inventory?.inStock &&
+    productOld?.inventory?.quantity - payload?.quantity >= 0
+  ) {
+    const newQuantity = productOld.inventory.quantity - payload.quantity;
+    console.log(newQuantity);
+    const result = await Order.create(payload);
+    await Product.findByIdAndUpdate(productId, {
+      'inventory.quantity': newQuantity,
+      'inventory.inStock': !(newQuantity === 0),
+    });
+    return result;
+  } else {
+    return;
+  }
 };
 
 // const retrieveAllOrders = async () => {
